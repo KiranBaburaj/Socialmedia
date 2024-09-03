3dde81cf9d072bc5b2d1ad18c5c40692f8bffffb
kiranbaburaj <kiranbaburaj.t@gmail.com>
2024-09-03 14:51:48 +0530

    Initial commit

---
+++ socialmedia/accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer
+from rest_framework.permissions import AllowAny
+
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
