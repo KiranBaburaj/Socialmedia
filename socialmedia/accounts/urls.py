c9caba66271ef40986845cf89b0b258fc61d3d7d
kiranbaburaj <kiranbaburaj.t@gmail.com>
2024-09-03 17:10:43 +0530

    login details

--- socialmedia/accounts/urls.py
+++ socialmedia/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView,CustomLoginView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -7,6 +7,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', CustomLoginView.as_view(), name='custom_login'),
+
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- socialmedia/accounts/views.py
+++ socialmedia/accounts/views.py
@@ -5,6 +5,8 @@ from rest_framework import generics
 from django.contrib.auth import get_user_model
 from .serializers import RegisterSerializer
 from rest_framework.permissions import AllowAny
+from .serializers import CustomTokenObtainPairSerializer
+
 
 User = get_user_model()
 
@@ -12,3 +14,43 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
+
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from .serializers import RegisterSerializer
+from django.contrib.auth import get_user_model
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class CustomLoginView(generics.GenericAPIView):
+    permission_classes = [AllowAny]
+    
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            response_data = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+                'id': user.id,  # Include user ID
+                'username': user.username,
+                'email': user.email,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+            }
+            return Response(response_data, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
